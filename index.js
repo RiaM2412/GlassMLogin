6492100f843b1a889e344e66e24288e8e187a652
Riya Mishra <riamishra622@gmail.com>
2021-01-28 00:20:17 +0530

    second commit

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-// document.getElementById('login-btn').addEventListener('click', submit);
-// function submit(e){
-//     e.preventDefault();
-//     var email=getInputValue('input-box');
-//     var password=getInputValue('input-box2');
-//     //signin function
-//     firebase.auth().signInWithEmailAndPassword(email, password)
-//       .then((submit) => {
-//         // Signed in
-//         var user = submit.email;
+document.getElementById('login-btn').addEventListener('click', submit);
+function submit(e){
+    e.preventDefault();
+    var email=getInputValue('input-box');
+    var password=getInputValue('input-box2');
+    //signin function
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((submit) => {
+        // Signed in
+        var user = submit.email;
          
-//     alert('logged in');
-//     // document.location ='useraccess.html';
-//     //header("location:useraccess.html"); 
+    alert('logged in');
+    // document.location ='useraccess.html';
+    //header("location:useraccess.html"); 
     
-//         // ...
-//       })
-//       .catch((error) => {
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//       });
+        // ...
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
     
-//     // return document.getElementById('useraccess');
-//     }
-//     function getInputValue(id){
-//         return document.getElementById(id).value;
-//       }
+    // return document.getElementById('useraccess');
+    }
+    function getInputValue(id){
+        return document.getElementById(id).value;
+      }
--- signup.js
+++ signup.js
@@ -1,31 +1,31 @@
-// document.getElementById('Signup-btn').addEventListener('click', register);
-// function register(e){
+document.getElementById('Signup-btn').addEventListener('click', register);
+function register(e){
     
-//     e.preventDefault();
-//     var name=document.getElementByValue('label1').value;
-//     var email=getInputValue('label2');
-//     var password=getInputValue('label3');
+    e.preventDefault();
+    var name=getInputValue('input-box1');
+    var email=getInputValue('input-box2');
+    var password=getInputValue('input-box3');
 
-//     firebase.auth().createUserWithEmailAndPassword(email, password)
-//   .then((register) => {
-//     // Signed in 
-//     var user = register.name;
-//     firebase.firestore().collection('register').add({
-//         email: email,
-//         name:name,
-//         password:password,
-//         schoolName:schoolName,
-//         create:new Date()
-//     // ...
-//   }).then(res=>{alert('User Created');document.location ='index.html'}).catch(e=>console.log(e))
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((register) => {
+    // Signed in 
+    var user = register.name;
+    firebase.firestore().collection('user').add({
+        email: email,
+        name:name,
+        password:password,
+        create:new Date()
+    // ...
+  }).then(res=>{alert('User Created');document.location ='index.html'}).catch(e=>console.log(e))
  
 
-//   }).catch((error) => {
-//     var errorCode = error.code;
-//     var errorMessage = error.message;
-//     // ..
-//   });
-// function getInputValue(id){
-//     return document.getElementById(id).value;
-//   }
-// }
+  }).catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+
+}
+function getInputValue(id){
+    return document.getElementById(id).value;
+  }
